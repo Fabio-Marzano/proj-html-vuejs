8a5bd6b921d0a7897c95bebe67ce63aca2235449
fabiomarzano <fabiomarzano1993@gmail.com>
2024-08-31 11:53:42 +0200

    [UPD] install-bootstrap-and-fontawesome

---
+++ proj-html-vuejs/src/components/App.vue
@@ -0,0 +1,21 @@
+<script>
+  import AppHeader from "./AppHeader.vue";
+  export default {
+    components:{
+      AppHeader
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+
+</style>
