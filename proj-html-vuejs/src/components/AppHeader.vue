536054636563c176757b3b270b23b95b779bd197
fabiomarzano <fabiomarzano1993@gmail.com>
2024-09-01 12:55:00 +0200

    [UPD] add-card-image-for-courses

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -32,9 +32,7 @@
           <div class="col-4 head">
             <p class="m-1">Become an instructor</p>
             <p class="m-1">For enterprise</p>
-            <p class="m-1 text-blue"><u>Log in</u></p>
-            <img class="button-img" src="../assets/Img/hover-button1.png">
-            
+            <p class="m-1 text-blue"><u>Log in</u></p>            
           </div>
           
         </div>
@@ -46,9 +44,9 @@
           <div class="col-12">
             <div class="row">
               <div class="col-6">
-                <h1 class="font">Start investing in you</h1>
-                <p class="p-1">With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you</p>
-                <button type="button" class="btn btn-primary btn-sm">JOIN FOR FREE</button>
+                <h1 class="font margin-l mt-5">Start investing in you</h1>
+                <p class="p-1 margin-l">With over 1200 courses in 18 subjects, you're guaranteed to find something that's right for you</p>
+                <button type="button" class="btn btn-primary btn-sm margin-l">JOIN FOR FREE</button>
               </div>
               
               <div class="col-6">
@@ -61,12 +59,71 @@
           </div>
         </div>
       </div>
-      <div class="container">
+      <div class="container mt-4">
         <div class="row">
           <div class="col-12">
             <img class="img-2" src="../assets/Img/hover-card1.png">
           </div>
         </div>  
+      </div>
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-8">
+            <div class="col">
+    <div class="card h-100 ml-20">
+      <img src="../assets/Img/cat_1-1540x750.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Software Development</p>
+      </div>
+    </div>
+  </div>
+          </div>
+          <div class="col-4">
+            <div class="col">
+    <div class="card h-100">
+      <img src="../assets/Img/cat_2-272x161.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        
+        <p class="card-text">Art</p>
+      </div>
+    </div>
+  </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4">
+            <div class="col">
+    <div class="card h-100 ml-20 mt-3">
+      <img src="../assets/Img/cat_5-370x155.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        
+        <p class="card-text">Music</p>
+      </div>
+    </div>
+  </div>
+          </div>
+          <div class="col-4">
+            <div class="col">
+    <div class="card h-100 mt-3">
+      <img src="../assets/Img/cat_4-370x155.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        
+        <p class="card-text">Exercise</p>
+      </div>
+    </div>
+  </div>
+          </div>
+          <div class="col-4">
+            <div class="col">
+    <div class="card h-100 mt-3">
+      <img src="../assets/Img/cat_6-370x155.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">Photography</p>
+      </div>
+    </div>
+  </div>
+          </div>
+        </div>
       </div>
        
     </main>
